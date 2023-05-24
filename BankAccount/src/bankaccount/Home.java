0c2b5d6ce4d2b4087cccd132cc1f995be9f4967d
M Rifqan Zalbina <124742008+rifqanzalbina@users.noreply.github.com>
2023-05-24 17:00:42 +0700

    Add files via upload

---
+++ BankAccount/src/bankaccount/Home.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bankaccount;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class Home {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your 'Name' and 'CustomerId' to access your Bank account:");
+        String name=sc.nextLine();
+        String customerId=sc.nextLine();
+        BankAccount obj1=new BankAccount(name,customerId);
+        obj1.menu();
+    }
+    
+}
